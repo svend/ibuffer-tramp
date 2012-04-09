8adebc9fa998dd425629145ffa14ecf22895d429
Svend Sorensen <svend@ciffer.net>
2012-04-09 11:47:05 -0700

    Add code outline

---
+++ ibuffer-tramp.el
@@ -0,0 +1,13 @@
+;;; ibuffer-trampe.el --- Group ibuffer's list by TRAMP connection
+;;
+;;; Code:
+
+;; requires
+
+(require 'ibuffer)
+(require 'ibuf-ext)
+(eval-when-compile
+  (require 'cl))
+
+(provide 'ibuffer-tramp)
+;; ibuffer-tramp.el ends here
